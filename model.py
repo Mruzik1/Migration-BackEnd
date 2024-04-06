9017c959493e9df6dc3f30634ffbdcb67dda6644
Michael Ruzmetov <mimisha2014@gmail.com>
2024-04-06 14:42:17 +0200

    add model

---
+++ model.py
@@ -0,0 +1,28 @@
+from openai import OpenAI
+
+
+class Model:
+	def __init__(self, url="http://10.0.4.174:5555/v1"):
+		self.client = OpenAI(base_url=url, api_key="lm-studio")
+
+	def get_completion(self, model, message, temperature=0.7):
+		completion = self.client.chat.completions.create(
+			model=model,
+			messages=[
+				{"role": "user", "content": message}
+			],
+			temperature=temperature,
+		)
+
+		return completion.choices
+	
+	def get_response(self, message):
+		model_name = "TheBloke/deepseek-coder-6.7B-instruct-GGUF/deepseek-coder-6.7b-instruct.Q8_0.gguf"
+		completion = self.get_completion(model_name, message)
+		return completion[0].message.content
+
+
+if __name__ == "__main__":
+	model = Model()
+	response = model.get_response("import numpy as np")
+	print(response)
