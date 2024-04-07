67d81513db8e126e0dbd0d07a91e30b8cbc39674
Michael Ruzmetov <mimisha2014@gmail.com>
2024-04-07 11:15:24 +0200

    update test app

--- test_app.py
+++ test_app.py
@@ -1,21 +1,16 @@
 import requests
 
-# The URL of the Flask app endpoint you want to test
+
 url = "http://10.0.5.217:7676/code_migrator"
-
-# Define the JSON payload you want to send, which includes the filename
-# Adjust the payload to match the expected format of your Flask app
 payload = {
-    "filename": "migrated_code3.json"
+    "repo_url": "https://github.com/Mruzik1/Migration-Test.git",
+    "from_lang": "Vue.js",
+    "to_lang": "React",
+    "task_type": "describe_files"
 }
-
-# Send a POST request with JSON payload
 response = requests.post(url, json=payload)
 
-# Check the response
 if response.status_code == 200:
-    print("Success! Here's the response data:")
     print(response.json())
 else:
     print(f"Failed with status code: {response.status_code}")
-    print(f"Response content: {response.content}")
